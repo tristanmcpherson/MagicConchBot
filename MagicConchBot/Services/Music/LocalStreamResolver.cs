183f23dbb84ca8d67c3d304c3eb91a8ac4119130
IlmrynX <shredder8910@gmail.com>
2017-04-04 23:49:23 -0400

    Bug fix regarding input parsing, local path parsing

--- MagicConchBot/Services/Music/LocalStreamResolver.cs
+++ MagicConchBot/Services/Music/LocalStreamResolver.cs
@@ -7,12 +7,16 @@ namespace MagicConchBot.Services.Music
 {
     public class LocalStreamResolver : ISongResolver
     {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public async Task<string> GetSongStreamUrl(string uri)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
         {
             if (File.Exists(uri))
                 return new FileInfo(uri).FullName;
 
             var config = Configuration.Load();
+            if (string.IsNullOrEmpty(config.LocalMusicPath))
+                return null;
             Directory.CreateDirectory(config.LocalMusicPath);
             var file = Path.Combine(config.LocalMusicPath, uri);
             return File.Exists(file) ? file : null;
