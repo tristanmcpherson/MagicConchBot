5b601d449cc764274fb591e84002e86de9e9dd2c
IlmrynX <shredder8910@gmail.com>
2017-04-11 02:03:56 -0400

    PR Test 2

--- MagicConchBot/Attributes/RequireBotOwner.cs
+++ MagicConchBot/Attributes/RequireBotOwner.cs
@@ -9,7 +9,7 @@ namespace MagicConchBot.Attributes
     {
         public override async Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
         {
-            return Configuration.Load().Owners.Contains(context.User.Id) ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("This feature is no longer supported.");
+            return Configuration.Load().Owners.Contains(context.User.Id) ? PreconditionResult.FromSuccess() : PreconditionResult.FromError("This feature is no longer supported. ");
         }
     }
 }
