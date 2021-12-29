93458b48e9078f80d8524c1746301d2b18aec6d8
tristanmcpherson <shredder8910@gmail.com>
2021-12-28 23:50:01 -0500

    Fix dockerfile

--- MagicConchBot/Services/Music/MusicService.cs
+++ MagicConchBot/Services/Music/MusicService.cs
@@ -99,7 +99,6 @@ namespace MagicConchBot.Services.Music
                                     streamUri = uri;
                                     break;
                                 }
-                                
                             } 
                             catch
                             {
