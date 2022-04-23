49ba2385521a42b71d90d658be61c88c23c9f38f
tristanmcpherson <shredder8910@gmail.com>
2022-04-23 02:28:44 -0400

    Fix direct play maybe

--- MagicConchBot/Services/Music/DirectPlaySongResolver.cs
+++ MagicConchBot/Services/Music/DirectPlaySongResolver.cs
@@ -14,7 +14,7 @@ namespace MagicConchBot.Services.Music
 {
     public class DirectPlaySongResolver : ISongInfoService
     {
-        public Regex Regex => new(".(webm|mp3|avi|wav|mp4|flac)$");
+        public Regex Regex => new(@"(.+)\.(webm|mp3|avi|wav|mp4|flac)$");
 
         public Task<Song> GetSongInfoAsync(string url)
         {
