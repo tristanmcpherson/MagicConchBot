00262be639420ae1c00f0fc865f27458b32013eb
tristanmcpherson <shredder8910@gmail.com>
2023-08-04 16:13:35 -0400

    Add uptime

--- MagicConchBot/Modules/AdminModule.cs
+++ MagicConchBot/Modules/AdminModule.cs
@@ -1,73 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using Discord;
-using Discord.Commands;
+using Discord.Interactions;
 using MagicConchBot.Attributes;
-using MagicConchBot.Resources;
 
-namespace MagicConchBot.Modules {
-	[RequireBotOwner]
-	[Group("admin")]
-	public class AdminModule : ModuleBase {
-		[Command("reboot")]
-		public async Task Reboot() {
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-				await ReplyAsync("Rebooting bot host machine. Please wait approximately 1 minute, then ping using !info");
-
-				Process.Start("./scripts/reboot.sh");
-			}
-		}
-
-		[Command("restart")]
-		public async Task RestartBot() {
-			if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
-				await ReplyAsync("Restarting bot. Please wait.");
-
-				Process.Start("./scripts/restart.sh");
-			}
-		}
-	}
-
-    public class NoFun : ModuleBase
+namespace MagicConchBot.Modules
+{
+    [RequireBotOwner]
+	[Group("admin", "Admin commands")]
+	public class AdminModule : InteractionModuleBase<ConchInteractionCommandContext>
     {
-        [Command("nofun"), Alias("tristanhatesfun", "getfuckedidiot")]
-        public async Task Retard()
+        [SlashCommand("uptime", "Get the system uptime of the bot.")]
+        public async Task GetUptimeAsync()
         {
-            await ReplyAsync($"Stop whining you absolute retard {Context.User.Mention}");
+            var uptime = DateTimeOffset.Now - Process.GetCurrentProcess().StartTime;
+            var uptimeString = $"{uptime.Days}d {uptime.Hours}h {uptime.Minutes}m {uptime.Seconds}s";
+
+            await ReplyAsync($"Bot uptime: {uptimeString}");
         }
     }
-
-    /// TODO: Save these to config file
-	[RequireBotOwner]
-	[Group("admin blacklist")]
-	public class Blacklist : ModuleBase {
-		[Command("add")]
-		public Task AddToBlacklist(IUser user) {
-
-            //var config = Configuration.Load();
-            var blacklist = new List<ulong>(Configuration.Blacklist) {
-                    user.Id
-                };
-
-		    Configuration.Blacklist = blacklist.ToArray();
-		    Environment.SetEnvironmentVariable(Constants.BlacklistVariable,
-		        string.Join(',', blacklist.Select(x => x.ToString())));
-            return Task.CompletedTask;
-		}
-
-		[Command("remove")]
-		public Task RemoveFromBlacklist(IUser user) {
-            var blacklist = Configuration.Blacklist.Where(u => u != user.Id).ToArray();
-
-		    Configuration.Blacklist = blacklist.ToArray();
-
-            Environment.SetEnvironmentVariable(Constants.BlacklistVariable,
-		        string.Join(',', blacklist.Select(x => x.ToString())));
-            return Task.CompletedTask;
-		}
-	}
 }
